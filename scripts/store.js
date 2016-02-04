2ff972a03aa1a302a998d813c8527914bceb4e23
Matthias <matthias@rakam.eu>
2016-02-04 13:28:17 +0100

    add preloadImages()

--- scripts/store.js
+++ scripts/store.js
@@ -26,3 +26,11 @@ var StoreManager = {
     return _store.actors.concat(_store.movies);
   }
 }
+
+var preload = [
+  "https://image.tmdb.org/t/p/w500/fELdVq2KuqW8vWEeb1vi6IfDRSO.jpg", 
+  "https://image.tmdb.org/t/p/w500/7ad4iku8cYBuB08g9yAU7tHJik5.jpg", 
+  "https://image.tmdb.org/t/p/w500/eQs5hh9rxrk1m4xHsIz1w11Ngqb.jpg"
+];
+preloadImages(preload)
+  .then(() => console.log('all ok'));
