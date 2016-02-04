c6e88a46f9baecca955ebc6439dd6cb0f37aaacc
Luc Merceron <lmerceron22@gmail.com>
2016-02-04 08:20:09 +0100

    Adding store

---
+++ scripts/store.js
@@ -0,0 +1,26 @@
+let _store = {
+  actors: [],
+  movies: []
+}
+
+let StoreManager = {
+  getMovies: function(){
+    return _store.movies;
+  },
+
+  getActors: function(){
+    return _store.actors;
+  },
+
+  setMovies: function(movies){
+    _store.movies = movies;
+  },
+
+  setActors: function(actors){
+    _store.actors = actors;
+  },
+
+  getAll: function(){
+    return _store.actors.concat(_store.movies);
+  }
+}
